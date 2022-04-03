de34d27b0d84d031456578082c42a8be1752a213
ILikeTheCode <79585703+ILikeTheCode@users.noreply.github.com>
2022-04-03 19:38:40 +1000

    Implement error handling logic in API and client

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -12,6 +12,7 @@ export default function NavBar() {
                     🦀:D:D🦀
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities'/>
+                <Menu.Item as={NavLink} to='/errors' name='Errors Test Suite'/>
                 <Menu.Item>
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
