4c7ef9f5839bd2dd483619ed2b5cad32e8a51e80
ILikeTheCode <79585703+ILikeTheCode@users.noreply.github.com>
2022-04-16 20:30:11 +1000

    Add SignalR support to client. Add comment section

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -2,14 +2,15 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import { Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import ProfileAbout from "./ProfileAbout";
 import ProfilePhotos from "./ProfilePhotos";
 
 interface Props {
-    profile: Profile
+  profile: Profile;
 }
-export default observer(function ProfileContent({profile} : Props) {
+export default observer(function ProfileContent({ profile }: Props) {
   const panes = [
-    { menuItem: "About", render: () => <Tab.Pane>About Content</Tab.Pane> },
+    { menuItem: "About", render: () => <ProfileAbout /> },
     { menuItem: "Photos", render: () => <ProfilePhotos profile={profile} /> },
     { menuItem: "Events", render: () => <Tab.Pane>Events Content</Tab.Pane> },
     {
@@ -22,5 +23,11 @@ export default observer(function ProfileContent({profile} : Props) {
     },
   ];
 
-  return <Tab menu={{ fluid: true, vertical: true }} menuPosition='right' panes={panes} />;
-})
+  return (
+    <Tab
+      menu={{ fluid: true, vertical: true }}
+      menuPosition="right"
+      panes={panes}
+    />
+  );
+});
